0a148e52c2a5374fc59f7b94f0ee0e1507667c68
Evgeniy Salamakha <esalamaha@clevertec.ru>
2021-01-28 12:05:29 +0300

    homework 2 ready

---
+++ src/components/Layout/index.js
@@ -0,0 +1,30 @@
+import style from './style.module.css';
+
+const Layout = ({ id, title, descr, urlBg, colorBg }) => {
+    const backgroundStyle = {background: urlBg ? `url(${urlBg})` : colorBg}
+    console.log('backgroundStyle======= ', backgroundStyle);
+    
+
+    return (
+        <section class={style.root} id={id} style={backgroundStyle}>
+            <div class={style.wrapper}>
+                <article>
+                    {
+                        title && <div class={style.title}>
+                            <h3>{title}</h3>
+                            <span class={style.separator}></span>
+                        </div>
+                    }
+                    {
+                        descr && <div class={`${style.desc} ${style.full}`}>
+                            <p>{descr}</p>
+                        </div>
+                    }
+                    
+                </article>
+            </div>
+        </section>
+    );
+};
+
+export default Layout;
